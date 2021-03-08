5861badad64abaf21f1590d5917ff8b807ee6a5d
popovandrii1403@gmail.com <popovandrii1403@gmail.com>
2021-03-08 19:19:29 +0200

    fixed not importing db

--- app.py
+++ app.py
@@ -19,9 +19,7 @@ api = Api(app)
 jwt = JWT(app, authenticate, identity)
 app.config['JWT_EXPIRATION_DELTA'] = timedelta(seconds=1800)
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
+
 
 api.add_resource(Store, '/store/<string:name>')
 api.add_resource(Item, '/item/<string:name>')
